70e331497252ca6a8ffda8f4a4ba3827e5baf0e5
GustavoGran <gustavo.grans@gmail.com>
2020-07-09 23:27:44 -0300

    Added rule_sequence.py as an exercise to test loop substitution by recursive function calls and higher order functions

---
+++ rule_sequence.py
@@ -0,0 +1,26 @@
+
+def zero(s):
+    if s[0] == "0":
+        return s[1:]
+
+def one(s):
+    if s[0] == "1":
+        return s[1:]
+
+def print_state(string, rules):
+    print({ 'string' : string, 'rules' : rules})
+
+def rule_sequence(string, rules):
+
+    # print_state(string,rules)
+
+    if string != None and len(rules)>= 1:
+        return rule_sequence(rules[0](string),rules[1:])
+    else:
+        return string
+
+print(rule_sequence('0101', [zero, one, zero]))
+# => 1
+
+print(rule_sequence('0101', [zero, zero]))
+# => None
